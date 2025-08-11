236d184ffec73e56aa9fc1980011dc62ef00cef4
Chaim Lev-Ari <chiptus@gmail.com>
2025-08-11 11:26:15 +0100

    fix comments

--- src/components/Index/shared/SocialPlatformLinkList.tsx
+++ src/components/Index/shared/SocialPlatformLinkList.tsx
@@ -1,5 +1,4 @@
 import { SocialPlatformLink } from "./SocialPlatformLink";
-import { getSocialPlatformLogo } from "./SocialPlatformUtils";
 
 interface Artist {
   id: string;
@@ -17,25 +16,16 @@ export function SocialPlatformLinkList({
   artist,
   size = "md",
 }: SocialPlatformLinkListProps) {
-  const hasSpotify =
-    artist.spotify_url && getSocialPlatformLogo(artist.spotify_url);
-  const hasSoundcloud =
-    artist.soundcloud_url && getSocialPlatformLogo(artist.soundcloud_url);
-
-  if (!hasSpotify && !hasSoundcloud) {
-    return null;
-  }
-
   return (
     <div className="flex items-center gap-1">
-      {hasSpotify && artist.spotify_url && (
+      {artist.spotify_url && (
         <SocialPlatformLink
           url={artist.spotify_url}
           artistName={artist.name}
           size={size}
         />
       )}
-      {hasSoundcloud && artist.soundcloud_url && (
+      {artist.soundcloud_url && (
         <SocialPlatformLink
           url={artist.soundcloud_url}
           artistName={artist.name}
